c7500b3dd02d917712753ecfe0438ba6bb88746b
alliekameda <kameda@unlv.nevada.edu>
2024-10-10 01:31:58 -0700

    Create gap_follow_launch.py
    allison's version (yikes)

---
+++ launch/gap_follow_launch.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, TextSubstitution, PathJoinSubstitution
+import argparse
+
+parser = argparse.ArgumentParser(description='bubble:=#   the size of the safety bubble in meters\nmaxvel:=#   the maximum velocity of the vehicle in meters/sec')
+args = parser.parse_args()
+
+def generate_launch_description():
+	bubble_arg = DeclareLaunchArgument('bubble', default_value='0.0')
+	maxvel_arg = DeclareLaunchArgument('maxvel', default_value='0.0')
+
+	bubble = LaunchConfiguration('bubble')
+	maxvel = LaunchConfiguration('maxvel')
+
+	wall_node = Node(
+		package='gap_follow',
+		executable='gap_follow_node.py',
+		name='gap_follow_node',
+		# name=LaunchConfiguration('student'),
+		output='screen',
+		parameters=[
+			{'bubble': bubble},
+			{'maxvel': maxvel},
+		]
+    )
+
+	return LaunchDescription([
+        wall_node,
+		bubble_arg,
+		maxvel_arg,
+	])
