8e1b67b175bdcf17609bd137035fb78fb8ba0df4
Aviendha <aviendha.aviendha@gmail.com>
2024-10-17 15:22:33 -0700

    check

--- launch/gap_follow_launch.py
+++ launch/gap_follow_launch.py
@@ -3,32 +3,92 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, TextSubstitution, PathJoinSubstitution
-import argparse
-
-parser = argparse.ArgumentParser(description='bubble:=#   the size of the safety bubble in meters\nmaxvel:=#   the maximum velocity of the vehicle in meters/sec')
-args = parser.parse_args()
 
 def generate_launch_description():
-	bubble_arg = DeclareLaunchArgument('bubble', default_value='0.0')
-	maxvel_arg = DeclareLaunchArgument('maxvel', default_value='0.0')
+	# bubble_arg = DeclareLaunchArgument('bubble', default_value='0.0')
+	# maxvel_arg = DeclareLaunchArgument('maxvel', default_value='0.0')
 
-	bubble = LaunchConfiguration('bubble')
-	maxvel = LaunchConfiguration('maxvel')
+	# bubble = LaunchConfiguration('bubble')
+	# maxvel = LaunchConfiguration('maxvel')
 
-	wall_node = Node(
-		package='gap_follow',
-		executable='reactive_node.py',
-		name='reactive_node',
+	gap_node = Node(
+        package='gap_follow',
+        executable='reactive_node.py',
+        name='wall_follow_node',
 		# name=LaunchConfiguration('student'),
 		output='screen',
-		parameters=[
-			{'bubble': bubble},
-			{'maxvel': maxvel},
-		]
-	)
+		# parameters=[
+		# 	{'bubble': bubble},
+		# 	{'maxvel': maxvel},
+		# ]
+    )
 
 	return LaunchDescription([
-        wall_node,
-		bubble_arg,
-		maxvel_arg,
+        gap_node,
+		# bubble_arg, 
+		# maxvel_arg, 
 	])
+
+
+
+
+# #!/usr/bin/env python3
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+
+# def generate_launch_description():
+#     return LaunchDescription([
+#         Node(
+#             package='wall_follow',
+#             executable='wall_follow_node.py',
+#             name='wall_follow_node'
+#         ),
+#     ])
+# #!/usr/bin/env python3
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+
+# def generate_launch_description():
+#     return LaunchDescription([
+#         Node(
+#             package='gap_follow',
+#             executable='reactive_node.py',
+#             name='wall_follow_node'
+#         ),
+#     ])
+
+# #!/usr/bin/env python3
+# from launch import LaunchDescription
+# from launch.actions import DeclareLaunchArgument
+# from launch_ros.actions import Node
+# from launch.substitutions import LaunchConfiguration, TextSubstitution, PathJoinSubstitution
+# import argparse
+
+# parser = argparse.ArgumentParser(description='bubble:=#   the size of the safety bubble in meters\nmaxvel:=#   the maximum velocity of the vehicle in meters/sec')
+# args = parser.parse_args()
+
+# def generate_launch_description():
+# 	bubble_arg = DeclareLaunchArgument('bubble', default_value='0.0')
+# 	maxvel_arg = DeclareLaunchArgument('maxvel', default_value='0.0')
+
+# 	bubble = LaunchConfiguration('bubble')
+# 	maxvel = LaunchConfiguration('maxvel')
+
+# 	gap_node = Node(
+# 		package='gap_follow',
+# 		executable='reactive_node.py',
+# 		name='reactive_node',
+# 		# name=LaunchConfiguration('student'),
+# 		output='screen',
+# 		parameters=[
+# 			{'bubble': bubble},
+# 			{'maxvel': maxvel},
+# 		]
+# 	)
+
+# 	return LaunchDescription([
+#         gap_node,
+# 		bubble_arg,
+# 		maxvel_arg,
+# 	])
+
