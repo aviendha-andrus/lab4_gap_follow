4abf68793d173a59118728df23c20d7495cc44b2
aviendha-andrus <159486117+aviendha-andrus@users.noreply.github.com>
2024-11-01 01:16:13 -0700

    Update gap_follow_launch.py - cmd line arg not working

--- launch/gap_follow_launch.py
+++ launch/gap_follow_launch.py
@@ -5,30 +5,29 @@ from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, TextSubstitution, PathJoinSubstitution
 import argparse
 
-parser = argparse.ArgumentParser(description='bubble:=#   the size of the safety bubble in meters\nmaxvel:=#   the maximum velocity of the vehicle in meters/sec')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='bubble:=#   the size of the safety bubble in meters\nmaxvel:=#   the maximum velocity of the vehicle in meters/sec')
+# args = parser.parse_args()
 
 def generate_launch_description():
-	bubble_arg = DeclareLaunchArgument('bubble', default_value='0.0')
-	maxvel_arg = DeclareLaunchArgument('maxvel', default_value='0.0')
+	bubble_arg = DeclareLaunchArgument('b', default_value='0.0')
+	maxvel_arg = DeclareLaunchArgument('v', default_value='0.0')
 
-	bubble = LaunchConfiguration('bubble')
-	maxvel = LaunchConfiguration('maxvel')
+	bubble = LaunchConfiguration('b')
+	maxvel = LaunchConfiguration('v')
 
-	wall_node = Node(
+	gap_node = Node(
 		package='gap_follow',
 		executable='reactive_node.py',
 		name='reactive_node',
-		# name=LaunchConfiguration('student'),
 		output='screen',
 		parameters=[
-			{'bubble': bubble},
-			{'maxvel': maxvel},
+			{'b': bubble},
+			{'v': maxvel},
 		]
 	)
 
 	return LaunchDescription([
-        wall_node,
+        gap_node,
 		bubble_arg,
 		maxvel_arg,
 	])
